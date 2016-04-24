414ac9e1d86eed475d76939e18df3a6e5df5862d
Daan van Berkel <info@daanvanberkel.nl>
2016-04-24 14:37:37 +0200

    Oude backend verwijderd, omdat er nu een 'ingebouwde' backend is

--- backend/application/views/authentication/confirm/success.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="alert alert-success">Uw account is geactiveerd. U kunt nu <a href="<?php echo base_url('authentication'); ?>">inloggen</a>.</p>
-        </div>
-    </div>
-</div>
